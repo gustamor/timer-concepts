c9ff2d5a729c555d3a323e36c93f0f2cfa31cb19
Gustavo Moreno <gustavo.moreno.fernandez@gmail.com>
2023-09-25 00:07:59 +0200

    added startCountDownSystemClock and startCountDownWorkManager

--- app/src/main/java/es/gustavomoreno/timerflow/MainActivity.kt
+++ app/src/main/java/es/gustavomoreno/timerflow/MainActivity.kt
@@ -1,8 +1,16 @@
 package es.gustavomoreno.timerflow
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
 import es.gustavomoreno.timerflow.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -34,8 +42,11 @@ class MainActivity : AppCompatActivity() {
         binding.fabMain.setOnClickListener {
             GlobalScope.launch {
                 toggleIsRunning()
-                // startCountDownWorkDelay()
-                startCountDownTimerScheduler()
+                //startCountDownWorkDelay()
+                //startCountDownTimerScheduler()
+                //startCountDownTimerClock()
+                //startCountDownSystemClock()
+                startCountDownWorkManager()
             }
 
         }
@@ -64,7 +75,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startCountDownTimerScheduler() {
+    fun startCountDownTimerScheduler() {
         val timer = Timer()
 
         val task = object : TimerTask() {
@@ -72,16 +83,12 @@ class MainActivity : AppCompatActivity() {
             override fun run() {
                 if (isRunning) {
                     if (current > 0) {
-                        Log.d("sec", current.toString())
-
                         runOnUiThread {
                             binding.txtMain.text = current.toString()
                         }
                         current--
                     } else {
                         current = 0
-                        Log.d("sec", "0")
-
                         runOnUiThread {
                             binding.txtMain.text = current.toString()
                         }
@@ -95,9 +102,86 @@ class MainActivity : AppCompatActivity() {
         timer.scheduleAtFixedRate(task, 0, 1000)
     }
 
-    private fun startCountDownSystemClock() {}
+    private fun startCountDownTimerClock() {
+        var elapsed = duration
+        if (isRunning) {
+            runOnUiThread {
+                object : CountDownTimer(duration.toLong() * 1000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        isRunning = true
+                        elapsed--
+                        binding.txtMain.text = elapsed.toString()
+                    }
 
-    private fun startCountDownWorkManager() {}
+                    override fun onFinish() {
+                        elapsed = 0
+                        isRunning = false
+                        binding.txtMain.text = elapsed.toString()
+                    }
+                }.start()
+            }
+        }
+
+    }
+
+    private fun currentTime(): Long {
+        return SystemClock.elapsedRealtime()
+    }
+
+    private suspend fun startCountDownSystemClock() {
+        var current = duration
+        val startedAt = currentTime()
+        runOnUiThread {
+            binding.txtMain.text = current.toString()
+        }
+        while (current > -1) {
+            val elapsedTime: Long =
+                startedAt - currentTime() // the time that passed after the last time the user clicked the button
+            if (elapsedTime % 1000 == 0.toLong()) {
+                delay(1)
+                current--
+                runOnUiThread {
+                    binding.txtMain.text = current.toString()
+                }
+            }
+        }
+    }
+
+    private fun startCountDownWorkManager() {
+        val workRequest = OneTimeWorkRequestBuilder<MyWorker>()
+            .setExpedited(OutOfQuotaPolicy.DROP_WORK_REQUEST)
+            .build()
+        toggleIsRunning()
+        WorkManager.getInstance(this).enqueue(workRequest)
+    }
+
+}
+
+class MyWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    override fun doWork(): Result {
+        val timer = Timer()
+        var current = 10
+        var isRunning = true
+        val task = object : TimerTask() {
+            override fun run() {
+                if (isRunning) {
+                    if (current > 0) {
+                        Log.d("sec", current.toString())
+                        current--
+                    } else {
+                        current = 0
+                        Log.d("sec", current.toString())
+
+                        timer.cancel()
+                        isRunning = false
+                        current = 10
+                    }
+                }
+            }
+        }
+        timer.scheduleAtFixedRate(task, 0, 1000)
+        return Result.success()
+    }
 
 }
 
